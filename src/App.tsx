59ece510ed3a56c0dd2fa51c400d4bdd67a8cd80
pv18 <pv9998286620@yandex.ru>
2022-08-18 18:48:54 +0300

    init counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import './index.scss';
+import {useState} from 'react';
 
-const App = () => {
-    return (
+function App() {
+  const [counter, setCounter] = useState<number>(0)
+
+  const clickPlus = () => setCounter(counter + 1)
+  const clickMinus = () => setCounter(counter - 1)
+
+  return (
+      <div className="App">
         <div>
-          
+          <h2>Счетчик:</h2>
+          <h1>0</h1>
+          <button className="minus">- Минус</button>
+          <button className="plus">Плюс +</button>
         </div>
-    );
-};
+      </div>
+  );
+}
 
 export default App;
